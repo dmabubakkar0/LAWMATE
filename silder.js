8f556d29bb4a51450c0f19113ae21f8aa6de6e8e
dmabubakkar <dmabubakkar06@gmail.com>
2024-03-16 14:23:58 +0600

    oky

--- silder.js
+++ silder.js
@@ -25,3 +25,37 @@ $(document).ready(function () {
     });
   });
   
+
+
+
+
+
+
+
+  $(document).ready(function () {
+    $(".madarcho").owlCarousel({
+      loop: true,
+      margin: 5,
+      responsiveClass: true,
+      autoplay: 200,
+      nav: false,
+      center: true,
+      items: 1,
+      responsive: {
+        0: {
+          items: 1,
+          nav: false,
+        },
+        600: {
+          items: 1,
+          nav: false,
+        },
+        1000: {
+          items: 1,
+          nav: false,
+          margin: 30,
+        },
+      },
+    });
+  });
+  
